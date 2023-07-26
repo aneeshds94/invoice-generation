1bd1c96f4b03342ac4e4820f284761984c9f88a9
Aneesh <aneeshds320@gmail.com>
2023-07-26 09:27:57 +0530

    Add header, total price

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ for filepath in filepaths:
     pdf.cell(w=30, h=10, txt=str(columns[3]), border=1)
     pdf.cell(w=30, h=10, txt=str(columns[4]), border=1, ln=1)
 
-
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
@@ -40,4 +39,17 @@ for filepath in filepaths:
         pdf.cell(w=30, h=10, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=10, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=10, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=10, txt="", border=1)
+    pdf.cell(w=60, h=10, txt="", border=1)
+    pdf.cell(w=30, h=10, txt="", border=1)
+    pdf.cell(w=30, h=10, txt="", border=1)
+    pdf.cell(w=30, h=10, txt=str(total_sum), border=1, ln=1)
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=10, txt=f"The total price is {total_sum}")
+
     pdf.output(f"PDFs/{filename}.pdf")
